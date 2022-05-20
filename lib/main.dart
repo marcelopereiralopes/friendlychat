bc3ef35f009cb7dc44436c1c8b4aa4fa6834118c
Amanda Louzada <amanda.louzada@nubank.com.br>
2022-05-20 14:50:09 -0300

    Create text widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,17 +20,52 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
     Key? key,
   }) : super(key: key);
 
   @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FriendlyChat'),
+        title: const Text('Friendly Chat'),
       ),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+                child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  const InputDecoration.collapsed(hintText: "Send a message"),
+            )),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            )
+          ],
+        ));
+  }
+
+  void _handleSubmitted(String text) {
+    log(text);
   }
 }
